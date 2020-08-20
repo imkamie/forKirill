52cf857983626182e6697151689a125f496896c4
imkamie <59199486+imkamie@users.noreply.github.com>
2020-08-20 19:24:06 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,9 @@
+let dynamicBlock = document.querySelector('.dynamic')
+let string = document.createElement('span');
+string.innerHTML = 'Динамическая строка';
+dynamicBlock.append(string);
+
+let unitClaim = document.querySelector('.unit-claim__number')
+let numberId = document.createElement('span');
+numberId.innerHTML = '1';
+unitClaim.append(numberId);
